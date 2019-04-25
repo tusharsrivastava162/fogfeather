144d21c6c6287b4cd12f0744c498551dfb860366
TusharSrivastava <tushar@foo.bar>
2019-04-25 14:21:14 +0530

    v0.0.2 saml integration

--- fogfeather/urls.py
+++ fogfeather/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import django_saml2_auth.views
 
 urlpatterns = [
+    path('saml2_auth/', include('django_saml2_auth.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('core.urls')),
