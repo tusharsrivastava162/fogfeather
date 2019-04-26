8cbe96db62ad9d7ad19ac00e038dd529e6891c62
TusharSrivastava <tushar@foo.bar>
2019-04-26 11:59:01 +0530

    v0.0.1 staticfiles fix

--- fogfeather/settings/base.py
+++ fogfeather/settings/base.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 #
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
