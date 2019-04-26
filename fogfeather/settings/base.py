3e4ce60cd969bee1f8aa5032dc660d3aca338593
TusharSrivastava <tushar@foo.bar>
2019-04-26 13:47:41 +0530

    v0.0.1 static fix

--- fogfeather/settings/base.py
+++ fogfeather/settings/base.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'fogfeather.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../../templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
