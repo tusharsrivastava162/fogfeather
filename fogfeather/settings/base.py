948b74c6de408a7ab8a0f4db37a5a835c3ee1f92
TusharSrivastava <tushar@foo.bar>
2019-05-08 16:27:08 +0530

    cleanup

--- fogfeather/settings/base.py
+++ fogfeather/settings/base.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_saml2_auth',
-    'social_django',
     'rest_framework',
     'crispy_forms',
     'core',
@@ -57,17 +56,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-AUTHENTICATION_BACKENDS = (
-    'social_core.backends.open_id.OpenIdAuth',  # for Google authentication
-    'social_core.backends.google.GoogleOpenId',  # for Google authentication
-    'social_core.backends.google.GoogleOAuth2',  # for Google authentication
-    # 'social_core.backends.github.GithubOAuth2',  # for Github authentication
-    # 'social_core.backends.facebook.FacebookOAuth2',  # for Facebook authentication
-    'django.contrib.auth.backends.ModelBackend',
-)
+# AUTHENTICATION_BACKENDS = (
+#     'django.contrib.auth.backends.ModelBackend',
+# )
 
 ROOT_URLCONF = 'fogfeather.urls'
 
@@ -153,8 +146,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '415315345110-hlpk92o435ouuib3vmrd8nnvgimcoaes.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_SECRET = 'zcUk8rW2zCVZr9OV0vPKYeKK'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
