31406c6a5ccec7193d9b378435729b04bfa7be3c
Tushar Srivastava <tusharsrivastava162@gmail.com>
2019-05-08 07:20:38 +0000

    onelogin

--- fogfeather/settings/base.py
+++ fogfeather/settings/base.py
@@ -159,7 +159,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 SAML2_AUTH = {
     # Metadata is required, choose either remote url or local file path
-    'METADATA_AUTO_CONF_URL': 'https://outlooksso.okta.com/app/exkie6pfvsrL6Q4rr356/sso/saml/metadata',
+    #'METADATA_AUTO_CONF_URL': 'https://outlooksso.okta.com/app/exkie6pfvsrL6Q4rr356/sso/saml/metadata',
+    'METADATA_AUTO_CONF_URL': 'https://app.onelogin.com/saml/metadata/31ccd229-4ed0-4d16-88b9-81e1cc76d69f',
     # 'METADATA_LOCAL_FILE_PATH': '[The metadata configuration file path]',
 
     # Optional settings below
@@ -172,17 +173,17 @@ SAML2_AUTH = {
         'SUPERUSER_STATUS': False,  # The superuser status for new users
     },
     'ATTRIBUTES_MAP': {  # Change Email/UserName/FirstName/LastName to corresponding SAML2 userprofile attributes.
-        'email': 'userName',
-        'username': 'userName',
-        'first_name': 'firstName',
-        'last_name': 'lastName',
+        'email': 'username',
+        'username': 'username',
+        'first_name': 'first_name',
+        'last_name': 'last_name',
     },
     # 'TRIGGER': {
     #     'CREATE_USER': 'path.to.your.new.user.hook.method',
     #     'BEFORE_LOGIN': 'path.to.your.login.hook.method',
     # },
-    'ASSERTION_URL': 'http://13.126.147.68:8800', # Custom URL to validate incoming SAML requests against
-    'ENTITY_ID': 'http://13.126.147.68:8800/saml2_auth/acs/', # Populates the Issuer element in authn request
+    'ASSERTION_URL': 'http://52.66.250.136:8800', # Custom URL to validate incoming SAML requests against
+    'ENTITY_ID': 'http://52.66.250.136:8800/saml2_auth/acs/', # Populates the Issuer element in authn request
     'USE_JWT': True, # Set this to True if you are running a Single Page Application (SPA) with Django Rest Framework (DRF), and are using JWT authentication to authorize client users
-    'FRONTEND_URL': 'http://13.126.147.68', # Redirect URL for the client if you are using JWT auth with DRF. See explanation below
+    'FRONTEND_URL': 'http://52.66.250.136', # Redirect URL for the client if you are using JWT auth with DRF. See explanation below
 }
--- fogfeather/settings/production.py
+++ fogfeather/settings/production.py
@@ -3,4 +3,4 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1']
 
-LOGIN_REDIRECT_URL = 'http://ec2-13-126-147-68.ap-south-1.compute.amazonaws.com:8800/auth/complete/google-oauth2/'
+#LOGIN_REDIRECT_URL = 'http://ec2-52-66-250-136.ap-south-1.compute.amazonaws.com:8800/auth/complete/google-oauth2/'
